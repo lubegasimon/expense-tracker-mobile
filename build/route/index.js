46d9680effc5e213b1708f28c4a7badd2b0e4777
lubegasimon <lubegasimon73@gmail.com>
2024-10-22 15:40:44 +0300

    change jsFiles outdir to build

--- build/route/index.js
+++ build/route/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var router = (0, express_1.Router)();
-router.get("/signup", function (req, res) {
-    res.send("signup page");
+router.get("/", function (req, res) {
+    res.send("Hello World!");
 });
 exports.default = router;
