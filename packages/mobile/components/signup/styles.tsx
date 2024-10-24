f3cc6dff9f22d63d1f98db48734009c1f6da2e86
lubegasimon <lubegasimon73@gmail.com>
2024-10-24 18:43:44 +0300

    create signup/style.tsx for shared signup styles

---
+++ packages/mobile/components/signup/styles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const inputStyle = StyleSheet.create({
+  input: {
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+});
+
+export default inputStyle;
