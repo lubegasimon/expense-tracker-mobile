cd0a072f38e2b6d0bf411d86727175e2d94b24a1
lubegasimon <lubegasimon73@gmail.com>
2024-10-24 17:11:11 +0300

    add SignupForm component

---
+++ packages/mobile/components/SignupForm.tsx
@@ -0,0 +1,37 @@
+import { Text, View, TextInput, StyleSheet } from "react-native";
+import { useState } from "react";
+
+export default function SignupForm() {
+  const [username, setUsername] = useState("Username");
+
+  return (
+    <View style={styles.view}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsername}
+        value={username}
+        placeholder="Enter Your Username"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+
+  appName: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 10,
+  },
+});
