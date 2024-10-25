fc10e8ad8aed6bee7b78954e04e9dcb7d3af3c5a
lubegasimon <lubegasimon73@gmail.com>
2024-10-25 15:46:07 +0300

    use session management middlewares

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,12 +1,23 @@
 import morgan from "morgan";
+import session from "express-session";
 
 import app from "./server";
 import indexRouter from "./route/index";
 import signupRouter from "./route/signup";
+import { redisStore } from "./middleware/redis";
+import { secret } from "./config";
 
 const port: number = 3000;
 
 app.use(morgan("tiny"));
+app.use(
+  session({
+    store: redisStore,
+    resave: false,
+    saveUninitialized: false,
+    secret,
+  }),
+);
 
 app.use("/", indexRouter);
 app.use("/signup", signupRouter);
