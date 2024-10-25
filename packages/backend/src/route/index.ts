1d6be6d285f99ba0163f83f7f0d8d1cf21b4d908
lubegasimon <lubegasimon73@gmail.com>
2024-10-25 11:54:01 +0300

    send json response on get requests to root

--- packages/backend/src/route/index.ts
+++ packages/backend/src/route/index.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.json({ message: "Hello World!" });
 });
 
 export default router;
