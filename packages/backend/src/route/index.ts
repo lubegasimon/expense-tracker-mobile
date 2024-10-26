068446d2c57ac9012bae96514c5764234a8fa228
lubegasimon <lubegasimon73@gmail.com>
2024-10-26 13:12:44 +0300

    add "Content-Type": "application/json" header

--- packages/backend/src/route/index.ts
+++ packages/backend/src/route/index.ts
@@ -2,6 +2,9 @@ import { Router, Request, Response } from "express";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
+  res.set({
+    "Content-Type": "application/json",
+  });
   res.json({ message: "Create account" });
 });
 
