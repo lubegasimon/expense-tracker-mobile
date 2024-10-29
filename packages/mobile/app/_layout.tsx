296c0eeadf38554584fe32c01ee55a45e417390c
lubegasimon <lubegasimon73@gmail.com>
2024-10-29 15:11:27 +0300

    do not explicitly add screens to Stack because expo automatically infer that basing on the directory layout

--- packages/mobile/app/_layout.tsx
+++ packages/mobile/app/_layout.tsx
@@ -1,11 +1,5 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="signup" />
-      <Stack.Screen name="index" />
-      <Stack.Screen name="userDashboard" />
-    </Stack>
-  );
+  return <Stack />;
 }
