52f284602903a3b65211fff677ff10f4dd51752b
lubegasimon <lubegasimon73@gmail.com>
2024-11-03 15:33:32 +0300

    move UserMode from db.ts

--- packages/backend/src/db/db.ts
+++ packages/backend/src/db/db.ts
@@ -1,22 +1,4 @@
-import { Sequelize, DataTypes } from "sequelize";
+import { Sequelize } from "sequelize";
 import { dbUrl } from "../config";
 
-const sequelize = new Sequelize(dbUrl);
-
-const User = sequelize.define("User", {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    primaryKey: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
-
-User.sync();
+export const sequelize = new Sequelize(dbUrl);
