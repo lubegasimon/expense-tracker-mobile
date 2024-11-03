ef51f3982d7ad6ea27cf35e09ba6241f3156a688
lubegasimon <lubegasimon73@gmail.com>
2024-11-03 15:28:35 +0300

    create UserModel

---
+++ packages/backend/src/user/model.ts
@@ -0,0 +1,72 @@
+import { DataTypes, Model } from "sequelize";
+import bcrypt from "bcrypt";
+import { sequelize } from "../db/db";
+
+const INVALID_USERANME_FORMAT =
+  "Username must be 5-10 characters long and can only contain letters, numbers, and underscores";
+
+export interface UserInstance extends Model<UserAttrs>, UserAttrs {}
+
+type UUID = string;
+
+export interface UserAttrs {
+  id?: UUID;
+  username: string;
+  email: string;
+  password: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export const UserModel = sequelize.define<UserInstance>(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4(),
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isValid(username: string) {
+          const regex = /^[0-9a-zA-Z_]{5,10}$/;
+          if (!regex.test(username)) {
+            throw new Error(INVALID_USERANME_FORMAT);
+          }
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [5, 256],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate(user) {
+        return hashPassword(user);
+      },
+    },
+  },
+);
+
+function hashPassword(user: UserAttrs) {
+  const saltRounds = 10;
+  return bcrypt.hash(user.password, saltRounds).then((hash) => {
+    user.password = hash;
+  });
+}
