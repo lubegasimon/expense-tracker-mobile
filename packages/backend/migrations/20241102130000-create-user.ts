a1913dd008ca85217c317765ca9fa30752d99bd7
lubegasimon <lubegasimon73@gmail.com>
2024-11-04 13:02:19 +0300

    fix: convert .js migration and seed file to .ts

---
+++ packages/backend/migrations/20241102130000-create-user.ts
@@ -0,0 +1,33 @@
+import { QueryInterface, DataTypes } from "sequelize";
+module.exports = {
+  async up(queryInterface: QueryInterface) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      username: {
+        type: DataTypes.STRING,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    });
+  },
+  async down(queryInterface: QueryInterface) {
+    await queryInterface.dropTable("Users");
+  },
+};
