36e7dd194de37c673769b1f9afe66d78753cf4ca
lubegasimon <lubegasimon73@gmail.com>
2024-11-06 14:44:41 +0300

    add: validate middleware and userSchema, userSchema is a representation of signup form data

---
+++ packages/backend/src/middleware/validation/schemas/userSchema.ts
@@ -0,0 +1,34 @@
+import { JSONSchemaType } from "ajv";
+
+interface User {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const userSchema: JSONSchemaType<User> = {
+  type: "object",
+  properties: {
+    /* Note: "username" is a custom format, see ../validate */
+    username: { type: "string", format: "username" },
+    email: { type: "string", format: "email" },
+    password: { type: "string", minLength: 5, maxLength: 256 },
+    confirmPassword: {
+      type: "string",
+      const: { $data: "1/password" } as unknown as string,
+    },
+  },
+  required: ["username", "email", "password", "confirmPassword"],
+  additionalProperties: false,
+  errorMessage: {
+    properties: {
+      username:
+        "Username must be 5-10 characters long and can only contain letters, numbers, and underscores",
+      password: "Password must NOT have fewer than 5 characters",
+      confirmPassword: "Passwords do not match",
+    },
+  },
+};
+
+export default userSchema;
