2b1ae88b0522027a7f2938b94a17bbe9bc64371c
lubegasimon <lubegasimon73@gmail.com>
2024-11-06 10:36:48 +0300

    update obsolete snapshot

--- packages/mobile/components/__tests__/SignupForm.tsx
+++ packages/mobile/components/__tests__/SignupForm.tsx
@@ -5,12 +5,12 @@ import SignupForm from "../signup/SignupForm";
 
 describe("<SignupForm/>", () => {
   test("Signup form contains username text input", () => {
-    render(
+    const tree = render(
       <NavigationContainer>
         <SignupForm />
       </NavigationContainer>,
     );
-
+    expect(tree).toMatchSnapshot();
     screen.getByPlaceholderText("Enter Your Username");
     screen.getByPlaceholderText("Enter Your Email");
     screen.getByPlaceholderText("Enter Your Password");
