cffd743bf89d7c4e386e1f51b9c25ec479790f5a
lubegasimon <lubegasimon73@gmail.com>
2024-11-14 10:37:06 +0300

    test: should fail if email alredy exists

--- packages/backend/src/test/user/signup.test.ts
+++ packages/backend/src/test/user/signup.test.ts
@@ -1,5 +1,7 @@
 import request from "supertest";
 import app from "./app";
+import { create } from "../../user/create";
+import { sequelize } from "../../db/db";
 
 describe("POST /signup", () => {
   describe("username edge cases", () => {
@@ -79,6 +81,36 @@ describe("POST /signup", () => {
       expect(response.body.error[0].message).toContain(
         'must match format "email"',
       );
+    });
+
+    it("should return 400 for invalid email format", async () => {
+      const response = await request(app).post("/signup").send({
+        username: "johndoe",
+        email: "",
+        password: "johndoe",
+        confirmPassword: "johndoe",
+      });
+      expect(response.status).toBe(400);
+      expect(response.body.error[0].message).toContain(
+        'must match format "email"',
+      );
+    });
+
+    beforeEach(() => sequelize.truncate());
+    afterEach(() => sequelize.truncate());
+    afterAll(() => sequelize.close());
+
+    it("should return 409 for an email that exists", async () => {
+      const user = {
+        username: "johndoe",
+        email: "john@example.com",
+        password: "johndoe",
+        confirmPassword: "johndoe",
+      };
+      await create(user);
+      const response = await request(app).post("/signup").send(user);
+      expect(response.status).toBe(409);
+      expect(response.body.error).toBe("Email already exists");
     });
   });
 
