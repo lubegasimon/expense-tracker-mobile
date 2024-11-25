240007f36a9e8a4cefd0cda7bf01ebc7aad4ac08
lubegasimon <lubegasimon73@gmail.com>
2024-11-25 20:22:42 +0300

    export create and findUserByEmail as main function, and document findUserByEmail

--- packages/backend/src/user/create.ts
+++ packages/backend/src/user/create.ts
@@ -1,5 +1,7 @@
 import { UserAttrs, UserInstance, UserModel } from "./model";
 
-export const create = (user: UserAttrs): Promise<UserInstance> => {
+const create = (user: UserAttrs): Promise<UserInstance> => {
   return UserModel.create(user);
 };
+
+export default create;
--- packages/backend/src/user/find.ts
+++ packages/backend/src/user/find.ts
@@ -1,8 +1,32 @@
 import { UserModel } from "./model";
 
-export const findUserByEmail = (email: string) => {
+/**
+ * We call this [findUserByEmail] twice during the signup process;
+ *  1. During the signup form validation.
+ *     This ensures that candidate's data is checked and not saved
+ *     in Redis. This save some Redis resources and time.
+ *  2. During retrieving candidate's data from redis and saving it in the DB.
+ *
+ * Signup Context:
+ * A candidate initiates a signup process and submits the form, it is then
+ * validated. If it passes the validation check, the candidate's data
+ * is saved in Redis until when the candidate confirms the email by submiting
+ * the valid verification code sent to it. (The candidate can request to resend
+ * the verification code).
+ *
+ * If the candidate submits the valid verification code, their data is
+ * retrieved from the Redis Store.
+ * At this point we perform another findUserByEmail query to the database.
+ * It is important to do so because it is possible that two candidates
+ * attempted to signup using the same email, but one verified the email
+ * before the other. So we don't want to endup with duplicate emails.
+ */
+
+const findUserByEmail = (email: string) => {
   return UserModel.findOne({
     where: { email },
     attributes: { exclude: ["createdAt", "updatedAt"] },
   });
 };
+
+export default findUserByEmail;
