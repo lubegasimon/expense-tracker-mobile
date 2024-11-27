3c03c80f597cb10ab165e85b514d21cbf800b11f
lubegasimon <lubegasimon73@gmail.com>
2024-11-27 18:50:17 +0300

    test: basic GET request to dashboard route

--- packages/backend/src/test/user/app.ts
+++ packages/backend/src/test/user/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import signupRouter from "../../router/user/signup";
 import verifyCodeRouter from "../../router/user/verifyCode";
+import dashboardRouter from "../../router/user/dashboard";
 
 const app = express();
 
 app.use(express.json());
 app.use("/signup", signupRouter);
 app.use("/signup/verify-code", verifyCodeRouter);
+app.use("/dashboard", dashboardRouter);
 
-/* exports express app to run on tests */
+/* exports express app for the tests */
 export default app;
