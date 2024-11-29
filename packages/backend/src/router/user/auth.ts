972d3c2eaf0c175ad7bea8a249d17dfe80d8374b
lubegasimon <lubegasimon73@gmail.com>
2024-11-29 10:03:34 +0300

    change: how verification code is saved in Redis

--- packages/backend/src/router/user/auth.ts
+++ packages/backend/src/router/user/auth.ts
@@ -8,7 +8,7 @@ function initializeSession(email: string, response: Response) {
   const JWT_SECRET = process.env.SECRET || "jwt-token";
   const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: "7 days" });
   response.append("Authorization", `Bearer ${token}`);
-  response.status(201).json({
+  return response.status(201).json({
     message: `Successfully signed in`,
     token,
   });
