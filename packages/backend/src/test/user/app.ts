1eea7aa597fcde8c89afb085d6d3fbcf6d0f694c
lubegasimon <lubegasimon73@gmail.com>
2024-11-29 11:20:43 +0300

    test resend-code route

--- packages/backend/src/test/user/app.ts
+++ packages/backend/src/test/user/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import signupRouter from "../../router/user/signup";
 import verifyCodeRouter from "../../router/user/verifyCode";
 import dashboardRouter from "../../router/user/dashboard";
+import resendCodeRouter from "../../router/user/resendCode";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use("/signup", signupRouter);
 app.use("/signup/verify-code", verifyCodeRouter);
 app.use("/dashboard", dashboardRouter);
+app.use("/resend-code", resendCodeRouter);
 
 /* exports express app for the tests */
 export default app;
---
+++ packages/backend/src/test/user/signup/resendCode.test.ts
@@ -0,0 +1,17 @@
+import request from "supertest";
+import app from "../app";
+
+describe("POST /resend-code", () => {
+  const EMAIL = "john@doe.com";
+
+  it("should return 200 if code is sent to email", async () => {
+    const response = await request(app).post("/resend-code").send({
+      email: EMAIL,
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBe(
+      "A new verification code has been sent to your email",
+    );
+  });
+});
