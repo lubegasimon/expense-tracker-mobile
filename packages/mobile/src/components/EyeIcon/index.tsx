5958235ed650123a1e443e461699b2192a3f135b
lubegasimon <lubegasimon73@gmail.com>
2024-12-06 11:18:44 +0300

    add: EyeIcon component

---
+++ packages/mobile/src/components/EyeIcon/index.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Pressable } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export interface ShowPasswordProps {
+  isShowPassword: boolean;
+  setShowPassword: () => void;
+}
+
+function EyeIcon({ isShowPassword, setShowPassword }: ShowPasswordProps) {
+  return (
+    <>
+      <Pressable style={style.icon} onPress={setShowPassword}>
+        <Ionicons
+          name={isShowPassword ? "eye" : "eye-off"}
+          size={25}
+          color="black"
+        />
+      </Pressable>
+    </>
+  );
+}
+
+const style = StyleSheet.create({
+  icon: {
+    position: "absolute",
+    right: 10,
+    padding: 4,
+  },
+});
+
+export default EyeIcon;
