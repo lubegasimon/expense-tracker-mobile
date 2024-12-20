bd025459814aba5d1dd0e5cf4d7d44c4c08896f8
lubegasimon <lubegasimon73@gmail.com>
2024-12-20 17:38:08 +0300

    fix mobile/src/app/index.tsx file

--- packages/mobile/src/app/index.tsx
+++ packages/mobile/src/app/index.tsx
@@ -1,35 +1,17 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
 import HomeScreen from "@/src/screens/HomeScreen";
-import OtpScreen from "../screens/OtpScreen";
-import SignupForm from "../screens/SignupScreen";
-
-// export default function Home() {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.appName}>Welcome to Expense tracker</Text>
-//       <Link href="/signup">
-//         <HomeScreen />
-//       </Link>
-//     </View>
-//   );
-// }
 
 export default function Home() {
   return (
-    <View>
-      <OtpScreen />
+    <View style={styles.container}>
+      <Text style={styles.appName}>Welcome to Expense tracker</Text>
+      <Link href="/signup">
+        <HomeScreen />
+      </Link>
     </View>
   );
 }
-
-// export default function Home() {
-//   return (
-//     <View style={styles.container}>
-//       <SignupForm />
-//     </View>
-//   );
-// }
 
 const styles = StyleSheet.create({
   appName: {
