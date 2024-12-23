635d9f24a8c639288b730e7d9353a69e43fc5929
lubegasimon <lubegasimon73@gmail.com>
2024-12-23 16:48:23 +0300

    fix bug causing signup screen to act as a link

--- packages/mobile/src/app/signup.tsx
+++ packages/mobile/src/app/signup.tsx
@@ -1,10 +1,5 @@
-import { Link } from "expo-router";
 import SignupForm from "@/src/screens/SignupScreen";
 
 export default function Signup() {
-  return (
-    <Link href="/verifyCode">
-      <SignupForm />
-    </Link>
-  );
+  return <SignupForm />;
 }
