8ee159ae016a3857bdfef9e0281f2fc12c1a1c82
lubegasimon <lubegasimon73@gmail.com>
2024-12-26 13:06:56 +0300

    load environment variables in config.ts

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -1,2 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.test" });
+
 export const secret = process.env.SECRET || "secret";
 export const dbUrl = process.env.DATABASE_URI || "postgres://";
