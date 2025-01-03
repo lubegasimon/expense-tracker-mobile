a64a376db412d6217e9a6a6e560b9a3984e7fc38
lubegasimon <lubegasimon73@gmail.com>
2025-01-03 12:28:05 +0300

    test: DELETE category operation

---
+++ packages/backend/src/test/category/unit/delete.test.ts
@@ -0,0 +1,30 @@
+import create from "../../../category/create";
+import { sequelize } from "../../../db/db";
+import { v4 as uuidv4 } from "uuid";
+import models from "../../../models";
+import deleteCategory from "../../../category/delete";
+
+const id = uuidv4();
+
+describe("DELETE category", () => {
+  afterAll(async () => await models.Category.destroy({ truncate: true }));
+  afterAll(() => sequelize.close());
+
+  it("should delete category", async () => {
+    const createdCategory = await create({
+      id,
+      name: "Water",
+      details: "Water bill",
+    });
+    expect(createdCategory).toHaveProperty("name", "Water");
+
+    const result = await deleteCategory(id);
+    expect(result).toEqual(1);
+  });
+
+  it("should return 0 if category with specified ID doesn't exist", async () => {
+    const invalidId = uuidv4();
+    const result = await deleteCategory(invalidId);
+    expect(result).toStrictEqual(0);
+  });
+});
