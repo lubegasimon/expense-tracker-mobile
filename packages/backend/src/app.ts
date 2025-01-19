3774e3bf968987009528e572a2078998ac622edd
lubegasimon <lubegasimon73@gmail.com>
2025-01-19 12:13:04 +0300

    centralize routers

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -3,21 +3,8 @@ import cookieParser from "cookie-parser";
 import app from "./server";
 import { sessionManager } from "./middleware/session";
 import { logger } from "./middleware/logger";
-import indexRouter from "./router/user/index";
-import signupRouter from "./router/user/signup";
-import verifyCode from "./router/user/verifyCode";
-import dashboardRouter from "./router/user/dashboard";
-import resendCodeRouter from "./router/user/resendCode";
-import loginRouter from "./router/user/login";
-import categoryRouter from "./router/category/index";
-import createCategoryRouter from "./router/category/create";
-import editCategoryRouter from "./router/category/edit";
-import deleteCategoryRouter from "./router/category/delete";
-import expenseRouter from "./router/expense/index";
-import createExpenseRouter from "./router/expense/create";
-import editExpenseRouter from "./router/expense/edit";
-import deleteExpenseRouter from "./router/expense/delete";
 import handleError from "./middleware/errorHandler";
+import routers from "./routers";
 
 const port = 3000;
 
@@ -26,21 +13,7 @@ app.use(sessionManager);
 app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/", indexRouter);
-app.use("/signup", signupRouter);
-app.use("/signup/verify-code", verifyCode);
-app.use("/dashboard", dashboardRouter);
-app.use("/signup/resend-code", resendCodeRouter);
-app.use("/login", loginRouter);
-app.use("/category", categoryRouter);
-app.use("/category/create", createCategoryRouter);
-app.use("/category", editCategoryRouter);
-app.use("/category", deleteCategoryRouter);
-app.use("/expense", expenseRouter);
-app.use("/expense/create", createExpenseRouter);
-app.use("/expense", editExpenseRouter);
-app.use("/expense", deleteExpenseRouter);
+app.use(routers);
 
 app.use(handleError);
 
