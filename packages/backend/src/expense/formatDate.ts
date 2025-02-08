67576074aec13e18de23b311a74583571206546f
lubegasimon <lubegasimon73@gmail.com>
2025-02-08 16:09:11 +0300

    ensure dates are formatted in UTC across ennvironments (system env and CI)

--- packages/backend/src/expense/formatDate.ts
+++ packages/backend/src/expense/formatDate.ts
@@ -1,7 +1,26 @@
 import { format, parse } from "date-fns";
 
 export const formatClientDate = (date: string | undefined) =>
-  date ? parse(date, "dd/MM/yyyy", new Date()) : undefined;
+  date
+    ? new Date(
+        Date.UTC(
+          parse(date, "dd/MM/yyyy", new Date()).getFullYear(),
+          parse(date, "dd/MM/yyyy", new Date()).getMonth(),
+          parse(date, "dd/MM/yyyy", new Date()).getDate(),
+        ),
+      )
+    : undefined;
 
 export const formatServerDate = (date: Date | undefined | null) =>
-  !(date instanceof Date) || !date ? undefined : format(date, "dd/MM/yyyy");
+  !(date instanceof Date) || !date
+    ? undefined
+    : format(
+        new Date(
+          Date.UTC(
+            date.getUTCFullYear(),
+            date.getUTCMonth(),
+            date.getUTCDate(),
+          ),
+        ),
+        "dd/MM/yyyy",
+      );
--- packages/backend/src/test/expense/unit/formatDate.test.ts
+++ packages/backend/src/test/expense/unit/formatDate.test.ts
@@ -6,7 +6,7 @@ import {
 describe("Format Date", () => {
   it("should convert client date string to server Date", () => {
     const date = formatClientDate("06/02/2025");
-    expect(date!.toISOString()).toBe("2025-02-05T21:00:00.000Z");
+    expect(date!.toISOString()).toBe("2025-02-06T00:00:00.000Z");
   });
 
   it("should convert undefined client date to undefined server Date", () => {
@@ -16,7 +16,7 @@ describe("Format Date", () => {
 
   it("should server Date to client date string", () => {
     const date = formatServerDate(new Date("2025-02-05T21:00:00.000Z"));
-    expect(date).toBe("06/02/2025");
+    expect(date).toBe("05/02/2025");
   });
 
   it("should convert undefined server date to undefined client Date", () => {
