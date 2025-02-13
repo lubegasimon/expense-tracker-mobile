1104298cbe567a67daf3bd5c56726ec0a5bee048
lubegasimon <lubegasimon73@gmail.com>
2025-02-13 11:46:01 +0300

    apply validateCreateExpenseRequest to expense/create route

--- packages/backend/src/expense/router/create.ts
+++ packages/backend/src/expense/router/create.ts
@@ -2,38 +2,43 @@ import { Request, Response, Router } from "express";
 import create from "../operations/create";
 import { findCategory } from "../../category/operations/find";
 import { formatClientDate, formatServerDate } from "../formatDate";
+import validateCreateExpenseRequest from "../../middleware/validation/expense/validate";
 
 const router = Router();
 
-router.post("/", async (request: Request, response: Response) => {
-  const { name, amount, details, createdAt, category } = request.body;
+router.post(
+  "/",
+  validateCreateExpenseRequest(),
+  async (request: Request, response: Response) => {
+    const { name, amount, details, createdAt, category } = request.body;
 
-  const categoryData =
-    category === undefined ? null : await findCategory(category);
+    const categoryData =
+      category === undefined ? null : await findCategory(category);
 
-  create({
-    name,
-    amount,
-    details,
-    createdAt: formatClientDate(createdAt),
-    categoryId: categoryData?.id,
-  })
-    .then((expense) =>
-      response.status(201).json({
-        message: `Expense successfully created`,
-        expense: {
-          ...expense.dataValues,
-          createdAt: formatServerDate(expense?.createdAt),
-          category,
-        },
-      }),
-    )
-    .catch((error) => {
-      console.error(`An error occurred while creating expense: ${error}`);
-      response.status(500).send({
-        message: "An error occurred while creating expense. Please try again",
+    create({
+      name,
+      amount,
+      details,
+      createdAt: formatClientDate(createdAt),
+      categoryId: categoryData?.id,
+    })
+      .then((expense) =>
+        response.status(201).json({
+          message: `Expense successfully created`,
+          expense: {
+            ...expense.dataValues,
+            createdAt: formatServerDate(expense?.createdAt),
+            category,
+          },
+        }),
+      )
+      .catch((error) => {
+        console.error(`An error occurred while creating expense: ${error}`);
+        response.status(500).send({
+          message: "An error occurred while creating expense. Please try again",
+        });
       });
-    });
-});
+  },
+);
 
 export default router;
