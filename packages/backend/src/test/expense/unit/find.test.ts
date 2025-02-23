06afcdeef6e9d5c91e7469f3b7bf9858118b070f
lubegasimon <lubegasimon73@gmail.com>
2025-02-23 19:50:26 +0300

    test: findExpenseByDate DB operation

--- packages/backend/src/test/expense/unit/find.test.ts
+++ packages/backend/src/test/expense/unit/find.test.ts
@@ -2,7 +2,10 @@ import { v4 as uuidv4 } from "uuid";
 import createExpense from "../../../expense/operations/create";
 import createCategory from "../../../category/operations/create";
 import { sequelize } from "../../../../db/db";
-import { findExpenseById } from "../../../expense/operations/find";
+import {
+  findExpenseByDate,
+  findExpenseById,
+} from "../../../expense/operations/find";
 import models from "../../../models";
 
 const id = uuidv4();
@@ -12,10 +15,9 @@ const expense = {
   name: "Personal loan",
   amount: 20,
   createdAt: new Date(),
-  updatedAt: new Date(),
 };
 
-describe("find expense", () => {
+describe("find expense by Id", () => {
   afterEach(() => models.Expense.destroy({ truncate: true }));
   afterAll(() => models.Category.destroy({ truncate: true, cascade: true }));
   afterAll(() => sequelize.close());
@@ -56,5 +58,60 @@ describe("find expense", () => {
     const invalidId = uuidv4();
     const result = await findExpenseById(invalidId);
     expect(result).toBeNull();
+  });
+});
+
+describe("find expense by Date", () => {
+  afterAll(() => models.Expense.destroy({ truncate: true }));
+  afterAll(() => sequelize.close());
+
+  const anotherExpense = {
+    id: uuidv4(),
+    name: "Internet",
+    amount: 20,
+    createdAt: new Date("2025-02-06T00:00:00.000Z"),
+  };
+
+  it("should return expenses created on specified date", async () => {
+    await createExpense(expense);
+    await createExpense(anotherExpense);
+    const result = await findExpenseByDate(new Date("2025-02-06"));
+    expect(
+      result.map((expense) => {
+        const { updatedAt, ...rest } = expense.dataValues;
+        return rest;
+      }),
+    ).toStrictEqual([
+      {
+        ...anotherExpense,
+        amount: "20.00",
+        details: null,
+        categoryId: null,
+        Category: null,
+      },
+    ]);
+  });
+
+  it("should return expenses for current date if date is not specified", async () => {
+    const result = await findExpenseByDate(undefined);
+    expect(
+      result.map((expense) => {
+        const { updatedAt, ...rest } = expense.dataValues;
+        return rest;
+      }),
+    ).toStrictEqual([
+      {
+        ...expense,
+        amount: "20.00",
+        details: null,
+        categoryId: null,
+        Category: null,
+      },
+    ]);
+  });
+
+  it("should return empty array if there are no expenses for the specified date", async () => {
+    const result = await findExpenseByDate(new Date("2024-01-02"));
+    expect(result).toStrictEqual([]);
   });
 });
