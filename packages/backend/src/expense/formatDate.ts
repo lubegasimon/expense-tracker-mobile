b284e68a87292ae766557c249b0f49ea24164073
lubegasimon <lubegasimon73@gmail.com>
2025-02-23 19:50:37 +0300

    docs/document formatServerDate and formatClientDate functions

--- packages/backend/src/expense/formatDate.ts
+++ packages/backend/src/expense/formatDate.ts
@@ -1,8 +1,17 @@
 import { format } from "date-fns";
 
+/**
+ * The `formatServerDate` function takes a `Date` object (or`undefined`)
+ * and returns a formatted string in `MM/dd/yyyy` format if a valid
+ * date is provided. If the input is not a `Date` object or is `undefined`,
+ * it returns `undefined`.
+ *
+ * **Note**: We don't expect input `date` to be `undefined` since
+ * `createdAt` is automatically generated on expense creation.
+ * @param date
+ * @returns `string | undefined`
+ */
 export const formatServerDate = (date: Date | undefined) =>
-  /* But we don't expect Date to be undefined since createdAt is
-automatically created on expense creation */
   !(date instanceof Date) || date === undefined
     ? undefined
     : format(
@@ -16,7 +25,27 @@ automatically created on expense creation */
         "MM/dd/yyyy",
       );
 
+/**
+ * The `formatClientDate` function takes a `Date` object and returns an
+ * array containing two `Date` objects representing the start and end
+ * of the given date in `UTC` time.
+ * @param date
+ * @returns `Date[]`
+ */
 export const formatClientDate = (date: Date) => {
+  /**
+   * `toUTC` helper function;
+   * - creates a new Date object in UTC by using Date.UTC, ensuring the
+   * time is properly set without relying on local timezone.
+   * - Accepts hours, minutes, seconds, and milliseconds to specify
+   * the exact time of day.
+   *
+   * @param hh hours
+   * @param mm minutes
+   * @param ss seconds
+   * @param ms milliseconds
+   * @returns Date
+   */
   const toUTC = (hh: number, mm: number, ss: number, ms: number) =>
     new Date(
       Date.UTC(
